3cdc3a908fe5dcd1764587630a816371aeef9caf
Kevin Liu <yuhliu@umich.edu>
2020-06-05 23:21:52 -0400

    #163 Complete

--- app/src/main/java/com/example/flickrbrowser/PhotoDetailActivity.java
+++ app/src/main/java/com/example/flickrbrowser/PhotoDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.flickrbrowser;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,10 +20,13 @@ public class PhotoDetailActivity extends BaseActivity {
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
         if (photo != null) {
             TextView photoTitle = findViewById(R.id.photo_title);
-            photoTitle.setText("Title: " + photo.getTitle());
+            Resources resources = getResources();
+            String text = resources.getString(R.string.photo_title_text, photo.getTitle());
+            photoTitle.setText(text);
+            // photoTitle.setText("Title: " + photo.getTitle());
 
             TextView photoTags = findViewById(R.id.photo_tags);
-            photoTags.setText("Tags: " + photo.getTags());
+            // photoTags.setText("Tags: " + photo.getTags());
 
             TextView photoAuthor = findViewById(R.id.photo_author);
             photoAuthor.setText(photo.getAuthor());
